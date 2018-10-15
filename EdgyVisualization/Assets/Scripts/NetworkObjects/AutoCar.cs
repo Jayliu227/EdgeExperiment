152bd398b330bd838a032fbd1a116b7754236670
Jay <jl7895@nyu.edu>
2018-10-14 20:02:04 -0400

    Finish DFS path finding and its visualization

--- EdgyVisualization/Assets/Scripts/NetworkObjects/AutoCar.cs
+++ EdgyVisualization/Assets/Scripts/NetworkObjects/AutoCar.cs
@@ -1,37 +1,110 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class AutoCar : EdgeClient {
 
+    // the point of the end of one specific path
     private Vector3 destination;
+    // the current next goint point
+    private Vector3 target;
     private float speed = 1f;
     private bool isWalking = false;
+    private bool isAutomatic = true;
+
+    private float coolDown;
+    private float tick = 0;
+    private Mutex mutex;
+    private Queue<Vector3> movementQueue;
 
     void Start () {
-        SetDestination(new Vector3(1, 2, 0));
+        SetupConnection();
+
+        mutex = new Mutex();
+        movementQueue = new Queue<Vector3>();
+
+        speed = UnityEngine.Random.Range(1f, 3f);
+        coolDown = UnityEngine.Random.Range(1f, 5f);
+
+        tick = coolDown;
     }
-	
-	void Update () {
-        if (isWalking)
+
+    void Update () {
+        tick -= Time.deltaTime;
+
+        // if there is anything in the queue and we're not on the way
+        if (isWalking && isAutomatic)
         {
-            transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * speed);
-            if (transform.position == destination)
+            transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
+            if (transform.position == target)
             {
                 isWalking = false;
             }
         }
+
+        mutex.WaitOne();
+        if (movementQueue.Count > 0 && !isWalking && isAutomatic)
+        {
+            // take a new destination
+            target = movementQueue.Dequeue();
+            isWalking = true;
+        }
+        mutex.ReleaseMutex();
+
+        /*
+        if(Input.GetKeyDown(KeyCode.F) && !isWalking)
+        {
+            RequestForPath(new Vector3(0, 3, 0));
+        }*/
+
+        if (tick < 0 && !isWalking)
+        {
+            RequestForPath(MapUploader.GetRandomPointOnMap());
+            tick = coolDown;
+        }
     }
 
-    private void SetDestination(Vector3 dest)
+    private void RequestForPath(Vector3 newTargetPosition)
     {
-        destination = dest;
-        isWalking = true;
+        Debug.Log("Request a path to " + newTargetPosition.x + " " + newTargetPosition.y);
+        SendMessage(CommandList.GetCommandCode(Command.FIND_PATH));
+        string newRequest = "";
+        newRequest += (int)transform.position.x + " " + (int)transform.position.y + " ";
+        newRequest += (int)newTargetPosition.x + " " + (int)newTargetPosition.y;
+        SendMessage(newRequest);
     }
 
     protected override void ProcessReponse(string commandCode, string reponse)
     {
-        throw new NotImplementedException();
+        if (commandCode.Equals(CommandList.GetCommandCode(Command.FIND_PATH)))
+        {
+            // decode reponse
+            string[] elements = reponse.Split(' ');
+            int numOfSteps = int.Parse(elements[0]);
+            
+            // +1 because there is a space character at the end
+            if ((numOfSteps * 2 + 1 + 1) != (elements.Length) || numOfSteps == 0)
+            {
+                Debug.Log("Reponse has invalid form.");
+                return;
+            }
+
+            mutex.WaitOne();
+            for (int i = 0; i < numOfSteps; i++)
+            {
+                // the input is: numOfSteps s1a s1b s2a s2b ...
+                int firstIndex = i * 2 + 1;
+                int secondIndex = i * 2 + 2;
+
+                int x = int.Parse(elements[firstIndex]);
+                int y = int.Parse(elements[secondIndex]);
+
+                movementQueue.Enqueue(new Vector3(x, y, 0));
+            }        
+
+            mutex.ReleaseMutex();
+        }
     }
 }
--- EdgyVisualization/Assets/Scripts/NetworkObjects/MapUploader.cs
+++ EdgyVisualization/Assets/Scripts/NetworkObjects/MapUploader.cs
@@ -6,45 +6,37 @@ public class MapUploader : EdgeClient {
     public GameObject grid;
     public GameObject carInstance;
     public Transform mapRoot;
-    MapData map;
+    private static MapData map;
+    private List<List<GameObject>> mapObjects;
 
     void Start ()
     {
-        //SetupConnection();
+        mapObjects = new List<List<GameObject>>();
+
+        SetupConnection();
         GenerateMap();
         DrawMap();
+
+        SendMessage(CommandList.GetCommandCode(Command.UPLOAD_MAP));
+        SendMessage(map.Stringify());
 
         // generate a car instance
         float carX = 1;
         float carY = 0;
         Vector3 pos = new Vector3(carX, carY, 0);
-        GameObject car = Instantiate(carInstance, pos, Quaternion.identity);
+        GameObject car1 = Instantiate(carInstance, pos, Quaternion.identity);
+        car1.transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.cyan;
+        GameObject car2 = Instantiate(carInstance, pos, Quaternion.identity);
+        car2.transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.magenta;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            SendMessage(CommandList.GetCommandCode(Command.ECHO));
-            SendMessage("Echo from client...");
-        }
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            SendMessage(CommandList.GetCommandCode(Command.UPLOAD_MAP));
-            SendMessage(map.Stringify());
-        }
-
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            SendMessage(CommandList.GetCommandCode(Command.FIND_PATH));
-            SendMessage("0 3 2 0");
-        }
     }
 
     protected override void ProcessReponse(string commandCode, string reponse)
     {
-        Debug.Log(commandCode);
         Debug.Log(reponse);
     }
 
@@ -52,12 +44,13 @@ public class MapUploader : EdgeClient {
     {
         List<List<int>> mapData = new List<List<int>>();
 
-        var a = new List<int>() { 1, 0, 0, 0 };
-        var b = new List<int>() { 0, 0, 0, 1 };
-        var c = new List<int>() { 0, 1, 0, 0 };
-        var d = new List<int>() { 1, 1, 0, 0 };
-        var e = new List<int>() { 0, 0, 0, 0 };
-        var f = new List<int>() { 0, 0, 1, 1 };
+        var a = new List<int>() { 1, 0, 0, 0, 0 };
+        var b = new List<int>() { 0, 0, 0, 1, 0 };
+        var c = new List<int>() { 0, 1, 0, 0, 0 };
+        var d = new List<int>() { 0, 0, 1, 0, 1 };
+        var e = new List<int>() { 0, 1, 0, 0, 1 };
+        var f = new List<int>() { 0, 0, 0, 0, 0 };
+        var g = new List<int>() { 1, 0, 1, 0, 1 };
 
         mapData.Add(a);
         mapData.Add(b);
@@ -65,6 +58,7 @@ public class MapUploader : EdgeClient {
         mapData.Add(d);
         mapData.Add(e);
         mapData.Add(f);
+        mapData.Add(g);
 
         map = new MapData("FirstMap", 1, mapData);
     }
@@ -88,6 +82,7 @@ public class MapUploader : EdgeClient {
 
         for (int i = 0; i < width; i++)
         {
+            List<GameObject> mapRowObj = new List<GameObject>();
             for(int j = 0; j < height; j++)
             {
                 int value = map.GetValue(i, j);
@@ -103,7 +98,21 @@ public class MapUploader : EdgeClient {
                 {
                     go.transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.yellow;
                 }
+
+                mapRowObj.Add(go);
             }
+            mapObjects.Add(mapRowObj);
         }
+    }
+
+    public static Vector3 GetRandomPointOnMap()
+    {
+        int width = map.GetWidth();
+        int height = map.GetHeight();
+
+        int randomX = UnityEngine.Random.Range(0, width);
+        int randomY = UnityEngine.Random.Range(0, height);
+
+        return new Vector3(randomX, randomY, 0);
     }
 }
